8e97b6a31468b2b0063009edda758e9e63613616
Charlotte.Hong <hunandy14@gmail.com>
2024-11-18 13:24:50 +0900

    優化

--- ScpExecutor.ps1
+++ ScpExecutor.ps1
@@ -86,12 +86,17 @@ scp -oIdentityFile=C:/Users/hunan/.ssh/id_ed25519 -oBatchMode=yes local_dir1/Fil
 scp -oIdentityFile=C:/Users/hunan/.ssh/id_ed25519 -oBatchMode=yes local_dir2/File2.txt chg@192.168.3.53:~/remote_dir2/File2.txt
 "@ -split "`n" 
 
-ScpExecutor RedHat79 Task1 Task2 Task3 Task4 | ForEach-Object -Begin { $i = 0 } -Process {
-    if ($_ -eq $testStr[$i]) {
-        Write-Host "$_" -ForegroundColor Green
-    } else {
-        Write-Host "$_" -ForegroundColor Red
-    }
-    $i++
+# 執行 SCP 命令並比對結果
+$results = ScpExecutor RedHat79 Task1 Task2 Task3 Task4
+
+
+# 比對實際輸出與預期結果
+for ($i = 0; $i -lt $results.Count; $i++) {
+    $isMatch = $results[$i] -eq $testStr[$i]
+    $color = if ($isMatch) { 'Green' } else { 'Red' }
+    
+    if (-not $isMatch) { Write-Host "預期結果: " -NoNewline -ForegroundColor DarkGray; Write-Host $testStr[$i] -ForegroundColor DarkGray }
+    Write-Host "實際結果: " -NoNewline -ForegroundColor DarkGray
+    Write-Host $results[$i] -ForegroundColor $color
 }
 
